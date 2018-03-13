1f8f315b1b34c0c302eaff8bd34dabfa717d57be
Sunpy <sunpy@osufx.com>
2018-03-13 19:51:03 +0100

    Null if getDifficulty is incorrect or not found

--- api/difficulty.py
+++ api/difficulty.py
@@ -5,5 +5,8 @@ from common.jsonFlaskFormat import jsonify
 
 def handle(request):
     ppy_data = urllib.request.urlopen('http://osu.ppy.sh/web/osu-getdifficulty.php?c[]={}'.format(request.args.get("c")))
-    data = ppyFormat.verticalSplit(ppy_data.read(), ["checksum", "mode", "mods", "diff_unified"])
+    ppy_str = ppy_data.read()
+    if len(ppy_str) <= 0:
+        return jsonify(None)
+    data = ppyFormat.verticalSplit(ppy_str, ["checksum", "mode", "mods", "diff_unified"])
     return jsonify(data)
