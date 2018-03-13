08c228613115b16b3b9d2ae0ae556e44146ae5d5
Sunpy <sunpy@osufx.com>
2018-03-13 18:22:33 +0100

    Easy null pass if hash was not found

--- api/hash.py
+++ api/hash.py
@@ -5,7 +5,7 @@ from common.jsonFlaskFormat import jsonify
 def handle(request):
     ppy_data = urllib.request.urlopen('http://osu.ppy.sh/web/osu-gethashes.php?s={}'.format(request.args.get("s")))
     ppy_str = ppy_data.read()
-    if len(ppy_str) > 1:
+    if len(ppy_str) > 3:
         data = ppyFormat.verticalSplit(ppy_str, ["res", "body_hash", "header_hash"])
         if len(data["body_hash"]) + len(data["header_hash"]) == 0:
             data = None
