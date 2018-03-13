087f888d8faa5ce37d0ac2b7350c1851ad2af524
Sunpy <sunpy@osufx.com>
2018-03-13 18:39:28 +0100

    Pass non-arrays

--- common/ppyFormat.py
+++ common/ppyFormat.py
@@ -1,10 +1,19 @@
 def verticalSplit(data, scema):
-    new_entries = []
-    entries = data.rstrip().split(b"\n")
-    for entry in entries:
-        values = entry.split(b"|")
-        new_entry = {}
+    data = data.rstrip() # Remove the extra newline (if it exists)
+
+    if b"\n" in data:
+        new_entries = []
+        entries = data.split(b"\n")
+        for entry in entries:
+            values = entry.split(b"|")
+            new_entry = {}
+            for i in range(0, len(values)):
+                new_entry[scema[i]] = values[i].decode("utf-8")
+            new_entries.append(new_entry)
+        return new_entries
+    else:
+        values = data.split(b"|")
+        entry = {}
         for i in range(0, len(values)):
-            new_entry[scema[i]] = values[i].decode("utf-8")
-        new_entries.append(new_entry)
-    return new_entries
+            entry[scema[i]] = values[i].decode("utf-8")
+        return entry
